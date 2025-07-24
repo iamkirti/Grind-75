b79ffaea540e140945007174517a8093e9f1d846
iamkirti <kirti.sahai02@gmail.com>
2025-07-24 10:51:54 +0530

    binary search

--- Week1/BinarySearch/js/index.js
+++ Week1/BinarySearch/js/index.js
@@ -4,5 +4,24 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    
+let beg=0;
+let last=nums.length-1;
+let mid=0;
+    while(beg<=last){
+        mid=(beg+last)/2;
+        if(target===nums[mid]){
+            return mid;
+        }else if(target>nums[mid]){
+            beg=mid+1;
+            
+        }else{
+            last=mid-1;
+        }
+
+    }
+    return -1;
 }
+
+let nums = [-1,0,3,5,9,12];
+let target = 9;
+console.log(search(nums,target));
